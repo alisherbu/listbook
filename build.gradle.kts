7161873b5c73c89da71db4330e8e743ee9410873
Alisher <alisherqazaqbaev@gmail.com>
2023-07-01 23:01:09 +0500

    version catalog and gradle to dsl

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+@file:Suppress(
+    "DSL_SCOPE_VIOLATION",
+    "UnstableApiUsage"
+)
+
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.parcelize) apply false
+}
