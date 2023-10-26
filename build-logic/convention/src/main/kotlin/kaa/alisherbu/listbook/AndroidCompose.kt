b04813030778d9fe7822b129c3bf6883b24da721
Alisher <alisherqazaqbaev@gmail.com>
2023-10-26 12:42:25 +0500

    upgrade gradle versions

--- build-logic/convention/src/main/kotlin/kaa/alisherbu/listbook/AndroidCompose.kt
+++ build-logic/convention/src/main/kotlin/kaa/alisherbu/listbook/AndroidCompose.kt
@@ -7,7 +7,7 @@ import com.android.build.api.dsl.CommonExtension
 import org.gradle.kotlin.dsl.dependencies
 
 internal fun Project.configureAndroidCompose(
-    commonExtension: CommonExtension<*, *, *, *>,
+    commonExtension: CommonExtension<*, *, *, *, *>,
 ) {
     commonExtension.apply {
         buildFeatures {
